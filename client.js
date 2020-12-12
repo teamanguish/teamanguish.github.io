66f72b812917185591fc492ea0cf8c5f2c77481b
teamanguish <75899067+teamanguish@users.noreply.github.com>
2020-12-12 15:26:24 +0100

    added pre-release files

---
+++ client.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    var links = [
+		{
+            name: 'shr1mp',
+            link: 'https://steamcommunity.com/id/shr1mpIe'
+        },
+		{
+            name: 'akira',
+            link: 'https://steamcommunity.com/id/deafthnote'
+        },
+    {
+            name: 'lai mai',
+            link: 'https://steamcommunity.com/id/D4IGGI3RRIE/'
+    },
+    ];
+
+    for(var i in links) {
+        var link = links[i];
+
+        $('#marquee').append('<a href="' + link.link + '" target="_blank">' + link.name + '</a>');
+
+        var link = $('#marquee').children('a').last();
+
+        if(i != links.length - 1) {
+            $('#marquee').append(' · ');
+        }
+    }
+
+    $('#marquee').marquee({
+        duration: 20000,
+        gap: 400,
+        delayBeforeStart: 0,
+        direction: 'left',
+        duplicated: true
+    });
+});
